dd42891379fc7868aeb84565da4327bdbb440d2b
Kirill <duscicat@gmail.com>
2021-11-17 07:17:06 +0000

    AddPaternsWithGitignore

---
+++ Adapter/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id #{Id}  Имя: {FirstName}  Фамилия: {LastName}  Отдел: {Department}";
+        }
+    }
+    /// <summary>
+    /// Этот класс представляет класс Adaptee. 
+    /// Предполагая, что это устаревший код, который поддерживает список сотрудников и имеет только одну
+    /// функцию - для возврата всех сотрудников сразу.
+    /// </summary>
+    public class RecordServer
+    {
+        private List<Employee> _employees;
+
+        public RecordServer()
+        {
+            InitializeEmployees();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _employees;
+        }
+
+        private void InitializeEmployees()
+        {
+            _employees = new List<Employee>
+        {
+            new Employee { Id = 1001, FirstName = "Иван" , LastName = "Иванов", Department = "Финансовый"},
+            new Employee { Id = 1002, FirstName = "Петр" , LastName = "Петров", Department = "Разработка"},
+            new Employee { Id = 1003, FirstName = "Михаил" , LastName = "Михайлов", Department = "Тестирование"},
+            new Employee { Id = 1004, FirstName = "Василий" , LastName = "Васильев", Department = "Тестирование"},
+            new Employee { Id = 1005, FirstName = "Максим" , LastName = "Максимов", Department = "Разработка"}
+        };
+        }
+    }
+    /// <summary>
+    /// Представляет IAdapter
+    /// Интерфейс позволит клиенту выбрать сотрудника с помощью employeeId
+    /// </summary>
+    public interface IEmployeeService
+    {
+        Employee GetEmployee(int employeeId);
+    }
+    /// <summary>
+    /// Представляет класс Adapter.
+    /// Этот класс создает экземпляр класса RecordServer и обслуживает клиента возвращая сотрудника на основе id.
+    /// </summary>
+    public class EmployeeService : IEmployeeService
+    {
+        RecordServer recordServer;
+
+        public EmployeeService()
+        {
+            recordServer = new RecordServer();
+        }
+
+        /// Этот метод извлекает список сотрудников с сервера записи
+        /// и возвращает сотрудника на основе employeeId
+        public Employee GetEmployee(int employeeId)
+        {
+            var allEmployees = recordServer.GetEmployees();
+            return allEmployees.FirstOrDefault(e => e.Id == employeeId);
+        }
+    }
+
+    //использует IEmployeeService и выбирает сотрудника на основе идентификатора
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IEmployeeService service = new EmployeeService();
+
+            var employee = service.GetEmployee(1001);
+            PrintEmployeeDetails(employee);
+
+            employee = service.GetEmployee(1004);
+            PrintEmployeeDetails(employee);
+
+            employee = service.GetEmployee(1020);
+            PrintEmployeeDetails(employee);
+
+            employee = service.GetEmployee(1002);
+            PrintEmployeeDetails(employee);
+
+            Console.Read();
+        }
+
+        static void PrintEmployeeDetails(Employee employee)
+        {
+            if (employee != null)
+                Console.WriteLine(employee);
+            else
+                Console.WriteLine("Сотрудник не найден");
+        }
+    }
+}
---
+++ Chain/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+
+namespace Chain
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            // Настройка цепочки обязанности
+            Approver vasya = new Director();
+            Approver petya = new VicePresident();
+            Approver ivan = new President();
+            vasya.SetSuccessor(petya);
+            petya.SetSuccessor(ivan);
+            // Создание и обработка запросов на покупку
+            Purchase p = new Purchase(2034, 350.00, "Supplies");
+            vasya.ProcessRequest(p);
+            p = new Purchase(2035, 32590.10, "Project X");
+            vasya.ProcessRequest(p);
+            p = new Purchase(2036, 122100.00, "Project Y");
+            vasya.ProcessRequest(p);
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Approver
+    {
+        protected Approver successor;
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+
+    public class Director : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 10000.0)
+            {
+                Console.WriteLine("{0} одобренный запрос# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    public class VicePresident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 25000.0)
+            {
+                Console.WriteLine("{0} одобренный запрос# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    public class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 100000.0)
+            {
+                Console.WriteLine("{0} одобренный запрос# {1}",
+                    this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine(
+                    "Запрос# {0} требуется исполнительная встреча!",
+                    purchase.Number);
+            }
+        }
+    }
+    /// <summary>
+    /// Класс детали запроса
+    /// </summary>
+    public class Purchase
+    {
+        int number;
+        double amount;
+        string purpose;
+        // Конструктор
+        public Purchase(int number, double amount, string purpose)
+        {
+            this.number = number;
+            this.amount = amount;
+            this.purpose = purpose;
+        }
+        // Получаем и задаем номер покупки
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        // Получаем и задаем цену покупки
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        // Получаем и задаем цель покупки
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+    }
+}
